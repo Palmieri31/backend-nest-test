b78abe2a67d4cc9ad8a70d1f2ff1a05870880020
Federico Palmieri <fedepalmie95@gmail.com>
2025-03-04 22:47:24 -0300

    fix: use partial type in UpdateFilmDto

--- src/films/dto/updateFilmDto.ts
+++ src/films/dto/updateFilmDto.ts
@@ -1,34 +1,4 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsOptional, IsString, IsInt, IsDateString } from 'class-validator';
+import { PartialType } from '@nestjs/swagger';
+import { CreateFilmDto } from './createFilmDto';
 
-export class UpdateFilmDto {
-  @ApiProperty()
-  @IsOptional()
-  @IsString()
-  title?: string;
-
-  @ApiProperty()
-  @IsOptional()
-  @IsInt()
-  episode_id?: number;
-
-  @ApiProperty()
-  @IsOptional()
-  @IsString()
-  opening_crawl?: string;
-
-  @ApiProperty()
-  @IsOptional()
-  @IsString()
-  director?: string;
-
-  @ApiProperty()
-  @IsOptional()
-  @IsString()
-  producer?: string;
-
-  @ApiProperty()
-  @IsOptional()
-  @IsDateString()
-  release_date?: string;
-}
+export class UpdateFilmDto extends PartialType(CreateFilmDto) {}
